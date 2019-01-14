c42fc28a1b5c7f67f8f0463a450960bf6de59bd1
DESKTOP-9TTC085\ASUS <934812816@qq.com>
2019-01-14 21:01:19 +0800

    跟新test新增test3

--- testGit/src/com/iotek/test.java
+++ testGit/src/com/iotek/test.java
@@ -2,6 +2,7 @@ package com.iotek;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("你好");
+        System.out.println("你好啊啊啊啊ppp");
+
     }
 }
--- testGit/src/com/iotek/test2.java
+++ testGit/src/com/iotek/test2.java
@@ -2,6 +2,6 @@ package com.iotek;
 
 public class test2 {
     public static void main(String[] args) {
-        System.out.println("haha");
+        System.out.println("haha"+1111111111);
     }
 }
